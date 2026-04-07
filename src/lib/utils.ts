ed43e1b563abaa455b7de0079977225cf40e90b2
Ubuntu <ubuntu@vps-27e5ee5b.vps.ovh.net>
2026-04-07 17:09:40 +0200

    Initialise portfolio Astro avec thème zaggonaut

---
+++ src/lib/utils.ts
@@ -0,0 +1,71 @@
+import { type CollectionEntry, getCollection } from "astro:content";
+
+type ContentCollectionName = "blog" | "project";
+
+/**
+ * Shortens a string by removing words at the end until it fits within a certain length.
+ * @param content the content to shorten
+ * @param maxLength the maximum length of the shortened content (default is 20)
+ * @returns a shortened version of the content
+ */
+export const getShortDescription = (content: string, maxLength = 20) => {
+  const splitByWord = content.split(" ");
+  const length = splitByWord.length;
+  return length > maxLength
+    ? `${splitByWord.slice(0, maxLength).join(" ")}...`
+    : content;
+};
+
+/**
+ * Processes the date of an article and returns a string representing the processed date.
+ * @param timestamp the timestamp to process
+ * @returns a string representing the processed timestamp
+ */
+export const processArticleDate = (date: Date) => {
+  const monthSmall = date.toLocaleString("default", { month: "short" });
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${monthSmall} ${day}, ${year}`;
+};
+
+let configCache: CollectionEntry<"configuration"> | null = null;
+
+/**
+ * Retrieves the configuration collection entry from the content directory.
+ * It checks if the configuration is already cached to avoid multiple reads.
+ * There can only be one configuration file, so it throws an error if there are multiple or none.
+ * @returns the configuration collection entry
+ */
+export const getConfigurationCollection = async (): Promise<
+  CollectionEntry<"configuration">
+> => {
+  if (configCache) return configCache;
+
+  const configs = await getCollection("configuration");
+  if (configs.length !== 1) {
+    throw new Error(
+      "Configuration file not found or multiple configuration files present.",
+    );
+  }
+  configCache = configs[0];
+  return configs[0];
+};
+
+export const getOptionalCollection = async <
+  TCollection extends ContentCollectionName,
+>(
+  collection: TCollection,
+  filter?: (entry: CollectionEntry<TCollection>) => boolean,
+): Promise<CollectionEntry<TCollection>[]> => {
+  try {
+    return await getCollection(collection, filter);
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      error.message.includes(`The collection "${collection}" does not exist or is empty`)
+    ) {
+      return [];
+    }
+    throw error;
+  }
+};
